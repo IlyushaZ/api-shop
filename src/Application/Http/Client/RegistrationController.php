527c610d7cc3de5a4c3771e3aa14a24203cd9466
IlyushaZ <ilyusha.god@yandex.ru>
2019-11-10 17:52:06 +0300

    Добавлен swagger, добавлена документация для регистрации, доработан makefile, добавлен readme

--- src/Application/Http/Client/RegistrationController.php
+++ src/Application/Http/Client/RegistrationController.php
@@ -3,6 +3,8 @@
 namespace App\Application\Http\Client;
 use App\Application\Cqs\Registration\Command\RegisterClientCommand;
 use App\Application\Cqs\Registration\Dto\RegistrationDto;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Swagger\Annotations as SWG;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -11,7 +13,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class RegistrationController
 {
     /**
+     * Регистрация покупателя.
+     *
      * @Route("", methods={"POST"})
+     *
+     * @SWG\Parameter(
+     *     name="Информация о регистрирующемся пользователе.",
+     *     in="body",
+     *     required=true,
+     *     type="application/json",
+     *     @SWG\Schema(type="object", ref=@Model(type=App\Application\Cqs\Registration\Dto\RegistrationDto::class))
+     * )
+     *
+     * @SWG\Response(
+     *     response=201,
+     *     description="Возвращает информацию о зарегистрированном пользователе.",
+     *     @SWG\Schema(type="object", ref=@Model(type=App\Application\Cqs\User\Common\Output\UserOutput::class))
+     * )
+     *
+     * @SWG\Tag(name="auth")
      */
     public function register(RegisterClientCommand $command, RegistrationDto $dto)
     {
